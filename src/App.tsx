be0b05acbf7c157cb2d99dc3119198ce2e472dd3
Pema Ngodup <pngodup123@gmail.com>
2025-12-08 14:41:11 +0000

    type for children prop

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal
-        title="Learn React + TS"
-        description="Learn it from the ground up"
-      />
-      ;
+      <CourseGoal title="Learn React + TS">
+        Learn react from beginning.
+      </CourseGoal>
     </main>
   );
 }
