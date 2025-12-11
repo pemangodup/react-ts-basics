f3e22efefa93550380c94a027f40e7925a9fb478
Pema Ngodup <pngodup123@gmail.com>
2025-12-11 19:29:35 +0000

    Learning to use type in useState with an array

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import CourseGoal from "./components/CourseGoal";
+import goalsImg from "../public/goals.jpg";
+import Header from "./components/header";
+import { useState } from "react";
 
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function addGoalHandler() {
+    setGoals([{ title: "Pema", description: "Htis is it.", id: 1 }]);
+  }
+
   return (
     <main>
+      <Header image={{ src: goalsImg, alt: "A list of goals." }}>
+        <h1>Your Course Goals</h1>
+      </Header>
       <CourseGoal title="Learn React + TS">
         Learn react from beginning.
       </CourseGoal>
+      <button onClick={addGoalHandler}>Click Me</button>
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -8,6 +8,20 @@ import { type PropsWithChildren } from "react";
 
 type CourseGoalProps = PropsWithChildren<{ title: string }>;
 
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
+
 export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
